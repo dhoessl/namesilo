665e0da986aab9d67dc1e7cb93489f140c1a6817
Dominic Hößl <dominic.hoessl@noris.de>
2026-04-13 13:44:44 +0200

    REWORK: use namesilo-py and make it a galaxy role

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-ATTENTION! This library seems to be broken for a while without me noticing. Working on a fixed release
+Role Name
+=========
+
+Create, delete and update Namesilo DNS Records from ansible playbook
+
+Requirements
+------------
+
+This Module requires [namesilo-py](https://github.com/dhoessl/namesilo-py) which will be installed by the main task itself, if not present as python module
+
+Role Variables
+--------------
+
+| var | value | default |
+| --- | ----- | ------- |
+| `namesilo_api_key` | Your namesilo Api key - should be placed in a vault | `None` |
+| `namesilo_domains` | list of domains to manage - example below | `[]` |
+
+`namesilo_domains` example
+------------------
+```
+---
+namesilo_domains:
+  - name: "example.com"
+    records:
+      - host: "namesilo-test"
+        type: "A"
+        value: "0.0.0.0"
+        ttl: "3600"
+      - host: ""
+        type: "MX"
+        value: "mail.example.com"
+        ttl: "3600"
+        distance: "5"
+...
+```
+
+Example Playbook
+----------------
+```
+---
+- hosts: 'localhost'
+  roles:
+    - role: 'namesilo'
+      tags: 'namesilo'
+...
+```
+License
+-------
+
+GPL-3.0
+
+Author Information
+------------------
+
+* Dominic Hößl <dhoessl@dhoessl.de>
+
--- library/namesilo_dns.py
+++ library/namesilo_dns.py
@@ -1,168 +1,50 @@
 #!/usr/bin/python3
 
-from ansible.module_utils.namesiloapiconnector import NamesiloApiWrapper as naw
 from ansible.module_utils.basic import AnsibleModule
+from api import NamesiloAPI, Domain, DomainRecordStateError
 
-
-class namesilo:
-    def __init__(self):
-        fields = {
-            "apikey": {
-                'type': "str",
-                'required': True,
-                'no_log': True
-            },
-            "domain": {
-                'type': "str",
-                'required': True
-            },
-            "type": {
-                'type': "str",
-                'default': 'A',
-                'choices': ['A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SRV']
-            },
-            "host": {
-                'type': "str",
-                'default': ''
-            },
-            "value": {
-                'type': "str",
-                'required': True
-            },
-            "ttl": {
-                'type': "int",
-                'default': 3600
-            },
-            "distance": {
-                'type': "int",
-                'default': 10
-            },
-            "state": {
-                'type': "str",
-                'default': "present",
-                'choices': ['present', 'absent']
-            }
+def main():
+    module_args = {
+        "api_key": {
+            "type": "str",
+            "required": True,
+            "no_log": True
+        },
+        "domains": {
+            "type": "list",
+            "required": True
         }
-        self.module = AnsibleModule(
-            argument_spec=fields,
-            supports_check_mode=False
-        )
-        result = {
-            'changed': False,
-            'host': self.module.params['host'] + '.' + self.module.params['domain'],
-            'value': self.module.params['value'],
-            'ttl': self.module.params['ttl'],
-            'type': self.module.params['type'],
-            'state': self.module.params['state']
-        }
-        if self.module.params['host'] == '':
-            result['host'] = self.module.params['domain']
-        if self.module.params['type'] == 'MX':
-            result['distance'] = self.module.params['distance']
+    }
 
-        self.ApiConnection = naw(self.module.params['apikey'])
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
 
-        # Request current DNS Entries
-        hosts = self.CheckforRecord()
-        # Check if the host is in the existing entries
-        # If thats the case just update its config
-        for host in hosts:
-            if host["host"] != result["host"]:
-                continue
-            if host["type"] != result["type"]:
-                continue
-            if self.module.params['state'] != "present":
-                self.change_settings(host, 'delete')
-            if (host["ttl"] != str(self.module.params["ttl"])
-                    or host["value"] != self.module.params["value"]):
-                self.change_settings(host, "update")
-            if self.module.params['type'] != 'MX':
-                self.module.exit_json(**result)
-            if host['distance'] == str(self.module.params['distance']):
-                self.module.exit_json(**result)
-            self.change_settings(host, 'update')
-        # if the host to change is not in the current entries and state is absent, delete it
-        if self.module.params['state'] == 'absent':
-            self.module.exit_json(**result)
-        # if some other keyword than "present" or "absent" is used -> fail
-        if self.module.params["state"] != "present":
-            self.module.fail_json(
-                msg='Could not determine what to do with provided information!',
+    result = {
+        "changed": False,
+        "original_domains": module.params["domains"],
+        "domains": None
+    }
+
+    api = NamesiloAPI(module.params["api_key"])
+
+    for domain in module.params["domains"]:
+        try:
+            namesilo_domain = Domain(domain["name"], api, domain["records"])
+        except DomainRecordStateError as e:
+            module.fail_json(
+                msg=f"{e}",
                 **result
             )
-        # if host is not found in existing entries and should be present -> create
-        self.AddRecord()
-        changed_result = dict(old_data={}, changed=True)
-        new_data = self.CheckforRecord()
-        for data in new_data:
-            if data["host"] != result["host"]:
-                continue
-            if data["type"] != self.module.params["type"]:
-                continue
-            if data["value"] != self.module.params["value"]:
-                continue
-            changed_result['new_data'] = data
-            changed_result['diff'] = data
-        self.module.exit_json(**changed_result)
+        for record in namesilo_domain.records:
+            if record["state"] in ["updated", "added", "removed"]:
+                result["changed"] = True
+        result["domains"].append({
+            "name": domain["name"],
+            "records": namesilo_domain.records
+        })
+    module.exit_json(**result)
 
-    def change_settings(self, old_host_data, action):
-        if action == 'update':
-            self.UpdateDns(old_host_data['record_id'])
-        elif action == 'delete':
-            self.DeleteRecord(old_host_data['record_id'])
-        result = dict(old_data=old_host_data, changed=True)
-        new_data = self.CheckforRecord()
-        for host in new_data:
-            if host['record_id'] == old_host_data['record_id']:
-                result['new_data'] = host
-        self.module.exit_json(**result)
-
-    def CheckforRecord(self) -> list:
-        api_result = self.ApiConnection.listRecords(
-            self.module.params['domain']
-        )
-        result = {}
-        # If no RR is set just output the result
-        if 'No DNS ' in api_result['reply']['detail']:
-            return []
-        # If call was not successfull print the error
-        if api_result['reply']['detail'] != 'success':
-            result['detail'] = api_result['reply']['detail']
-            self.module.fail_json(
-                msg='Could not fetch information from API',
-                **result
-            )
-        # Output the resource_record list
-        if isinstance(api_result["reply"]["resource_record"], dict):
-            return [api_result["reply"]["resource_record"]]
-        return api_result["reply"]["resource_record"]
-
-    def AddRecord(self):
-        self.ApiConnection.addRecord(
-            self.module.params['domain'],
-            self.module.params['type'],
-            self.module.params['host'],
-            self.module.params['value'],
-            str(self.module.params['distance']),
-            str(self.module.params['ttl'])
-        )
-
-    def DeleteRecord(self, rrid):
-        self.ApiConnection.deleteRecord(
-            self.module.params['domain'],
-            rrid
-        )
-
-    def UpdateDns(self, rrid):
-        self.ApiConnection.updateRecord(
-            self.module.params['domain'],
-            rrid,
-            self.module.params['host'],
-            self.module.params['value'],
-            str(self.module.params['distance']),
-            str(self.module.params['ttl'])
-        )
-
-
-if __name__ == '__main__':
-    namesilo_mod = namesilo()
+if __name__ == "__main__":
+    main()
